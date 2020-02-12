71b51c1ddb239a5eedc60fcdb060b38a7bfa7d5b
Student Cyber <StudentC@BenGurion.co.il>
2020-02-12 11:34:04 +0200

    Added source file

---
+++ src/Whos/Lobby.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Whos
+{
+    [Activity(Label = "Lobby")]
+    public class Lobby : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            SetContentView(Resource.Layout.activity_lobby);
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
---
+++ src/Whos/Shop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Whos
+{
+    [Activity(Label = "Shop")]
+    public class Shop : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            SetContentView(Resource.Layout.activity_shop);
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
