95f2c21d896a3c70a8024cd811015b4605f7e733
Hadar <hadarmanoron@gmail.com>
2020-02-28 12:12:06 +0200

    Added files - fixed problems

--- src/WhosGame/Lobby.cs
+++ src/WhosGame/Lobby.cs
@@ -15,24 +15,75 @@ namespace WhosGame
     [Activity(Label = "Lobby")]
     public class Lobby : Activity
     {
+        TextView PlayerTexts;
+        Dialog d;
+        //for the dialog
+        EditText playerName;
+        Button submitName;
+        List<string> names = new List<string>();
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_lobby);
+            PlayerTexts = (TextView)FindViewById(Resource.Id.playerText);
             bool isCreator = Intent.GetBooleanExtra("isCreator", false);
             if (isCreator)
             {
+                LinearLayout l2 = (LinearLayout)FindViewById(Resource.Id.linearl);
                 Button createBtn = new Button(this);
+                Button addPlayers = new Button(this);
+                LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(400, 100);
+                //create button for the game 
+                createBtn.LayoutParameters = lParams;
                 createBtn.SetHeight(100);
                 createBtn.SetWidth(100);
                 createBtn.Text = "Create Game";
-
+                l2.AddView(createBtn);
+                //add players
+                addPlayers.LayoutParameters = lParams;
+                addPlayers.SetHeight(100);
+                addPlayers.SetWidth(100);
+                addPlayers.Text = "Add Players";
+                l2.AddView(addPlayers);
+                addPlayers.Click += AddPlayers_Click;
+                createBtn.Click += CreateBtn_Click;
             }
             else
             {
                 //joining game
+                //for the multiplayer edition
             }
-            // Create your application here
+        }
+
+        private void CreateBtn_Click(object sender, EventArgs e)
+        {
+            Intent i = new Intent(this, typeof(Reveal));
+            i.PutStringArrayListExtra("namesList", names);
+            StartActivity(i);
+        }
+
+        private void AddPlayers_Click(object sender, EventArgs e)
+        {
+            AddUserDialog();
+        }
+
+        public void AddUserDialog()
+        {
+            d = new Dialog(this);
+            d.SetContentView(Resource.Layout.add_player);
+            d.SetCancelable(true);
+            playerName = (EditText)d.FindViewById(Resource.Id.namePlayer);
+            submitName = (Button)d.FindViewById(Resource.Id.clickName);
+            d.Show();
+            submitName.Click += SubmitName_Click;
+        }
+
+        private void SubmitName_Click(object sender, EventArgs e)
+        {
+            names.Add(playerName.Text);
+            PlayerTexts.Text += playerName.Text+"\n";
+            d.Dismiss();
         }
     }
 }
---
+++ src/WhosGame/Reveal.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+
+namespace WhosGame
+{
+    [Activity(Label = "Reveal")]
+    public class Reveal : Activity
+    {
+        TextView textRev;
+        List<string> names;
+        List<string> bad;
+        List<string> good;
+        LinearLayout l2;
+        Button revBtn;
+        Button retBtn;
+        string currPlayer;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            Random rand = new Random();
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_reveal);
+            textRev = (TextView)FindViewById(Resource.Id.currTXT);
+            //min 5 ?
+            names = Intent.GetStringArrayListExtra("namesList").ToList();
+            bad = new List<string>();
+            good = new List<string>();
+            int[] beenArr = new int[2];
+            int sizeList = names.Count;
+            //randomize 2 bad guys and other good guys
+            if(sizeList > 4 && sizeList < 8)
+            {
+                var n = rand.Next(0, sizeList);
+                for(int i = 0; i < 2; i++)
+                {
+                    beenArr[i] = n;
+                    bad.Append(names[n]); 
+                }
+                for(int i = 0; i < sizeList; i++) 
+                {
+                    if (!beenArr.Contains(i))
+                    {
+                        good.Append(names[i]);
+                    }
+                }
+            }
+            startRevealing();
+         
+        }
+        public void startRevealing()
+        {
+            l2 = (LinearLayout)FindViewById(Resource.Id.layoutrev1);
+            revBtn = new Button(this);
+            LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(400, 100);
+            //create button for the game 
+            revBtn.LayoutParameters = lParams;
+            revBtn.SetHeight(100);
+            revBtn.SetWidth(100);
+            revBtn.Text = "Reveal Yourself";
+            l2.AddView(revBtn);
+            foreach (var player in names)
+            {
+                currPlayer = player;
+                textRev.Text = "Player: " + player + "Your turn to see what you got!";
+                revBtn.LongClick += RevBtn_LongClickAsync;
+            }
+        }
+
+        private async void RevBtn_LongClickAsync(object sender, View.LongClickEventArgs e)
+        {
+            l2.RemoveView(revBtn);
+            l2 = (LinearLayout)FindViewById(Resource.Id.layoutrev1);
+            retBtn = new Button(this);
+            LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(400, 100);
+            //create button for the game 
+            retBtn.LayoutParameters = lParams;
+            retBtn.SetHeight(100);
+            retBtn.SetWidth(100);
+            retBtn.Text = "Next Player";
+            l2.AddView(retBtn);
+            if (bad.Contains(currPlayer))
+            {
+                textRev.Text = "Player: " + currPlayer + "You are BAD guy";
+            }
+            else
+            {
+                textRev.Text = "Player: " + currPlayer + "You are good guy";
+            }
+            await Button_Clicked();
+        }
+        private async Task Button_Clicked()
+        {
+            retBtn.LongClick += RetBtn_LongClick;
+        }
+        private void RetBtn_LongClick(object sender, View.LongClickEventArgs e)
+        {
+            return;
+        }
+    }
+}
